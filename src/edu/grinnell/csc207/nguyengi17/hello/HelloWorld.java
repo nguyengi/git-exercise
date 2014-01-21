f4042dda2d790ad11c91692820cc736c7a75473d
nguyengi <nguyengi17@grinnell.edu>
2014-01-21 11:18:39 -0600

    Update HelloWorld.java

--- src/edu/grinnell/csc207/nguyengi17/hello/HelloWorld.java
+++ src/edu/grinnell/csc207/nguyengi17/hello/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld
   public static void
     main (String[] args)
   {
-    System.out.println ("Hello, GitHub!");
+    System.out.println ("Hello, Git!");
     System.out.println ("Hello 2!");
   } // main(String[]) 2
 }
